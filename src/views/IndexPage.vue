98464f7fb275165ca4b7f1f87102a7f641b0df29
CodeGrinder123 <57931904+CodeGrinder123@users.noreply.github.com>
2023-07-17 23:15:09 +0800

    Added end/start margin

--- src/views/IndexPage.vue
+++ src/views/IndexPage.vue
@@ -21,7 +21,7 @@
 
     <h3 class="text-center text-white pt-3 pb-4">Our story</h3>
     <div class="container-fluid">
-      <div class="ms-1 me-2">
+      <div class="ms-1 me-3">
         <div class="d-flex justify-content-center align-items-center pb-3">
           <div class="row">
             <div class="col"></div>
